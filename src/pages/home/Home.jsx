c52a98f73fdf0afe2ec9b2d22f947b7820389a88
Maxime Vande <Maxime62580@laposte.net>
2023-06-26 11:51:05 +0200

    A

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,11 +9,11 @@ function Home() {
   return (
       <><Header/><Videohome/>
       <h1 className='h1home'>Maxime Vande Voorde développeur web</h1><div className='.photo-container'>
-        <p className='textehome'><img className="moi" src={process.env.PUBLIC_URL + '/images/moi.webp'} alt="Maxime Vande Voorde développeur web"></img>Bonjour et bienvenue sur mon site ! Je suis Maxime Vande Voorde, un développeur web junior polyvalent et motivé. Je suis actuellement en formation et je serai disponible pour travailler avec vous à partir de début juin.
+        <p className='textehome'><img className="moi" src={process.env.PUBLIC_URL + '/images/moi.webp'} alt="Maxime Vande Voorde développeur web"></img>Bienvenue sur mon site ! Je m'appelle Maxime Vande Voorde, et je suis ravi de vous présenter mon portfolio en tant que développeur web junior polyvalent. Je viens de terminer ma formation et j'ai obtenu mon diplôme de développeur web. À partir de septembre, je suis à la recherche d'une opportunité d'alternance dans le cadre d'un bac+3 avec My Digital School.
 
-        En tant que développeur web, je suis capable de m'adapter rapidement à n'importe quel langage de programmation et de concevoir des applications web robustes et évolutives. Je suis également à l'aise pour travailler en équipe, communiquer efficacement et respecter les délais.
+En tant que développeur web, je suis capable de m'adapter rapidement à n'importe quel langage de programmation et de concevoir des applications web robustes et évolutives. Je suis également à l'aise pour travailler en équipe, communiquer efficacement et respecter les délais.
 
-        Si vous êtes à la recherche d'un développeur web dynamique et motivé pour rejoindre votre entreprise, n'hésitez pas à me contacter. Je suis ouvert à toutes les opportunités et je suis impatient de discuter avec vous de vos besoins afin de pouvoir y répondre.</p>
+Si vous êtes à la recherche d'un développeur web dynamique et motivé pour rejoindre votre entreprise, n'hésitez pas à me contacter. Je suis ouvert à toutes les opportunités et je suis impatient de discuter avec vous de vos besoins afin de pouvoir y répondre.</p>
       </div><Footer/>
       </>
   )
